473b33a3f6da0886309da4d8d19ec9e4dcff2d44
Tran Binh Trieu <tranbinhtrieuhgbt2004@gmail.com>
2023-07-29 09:46:18 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+//React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//root
+import './index.css'
+import App from './App.tsx'
+//-------------------------------------------------------------------------------
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
